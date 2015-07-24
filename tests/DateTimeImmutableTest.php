9f427b44e10e84f1c9c4b452eb2f0f30ac66b3a4
joris.gar <joris.garonian@gmail.com>
2015-07-24 18:44:30 +1000

    Test DateTimeImmutable

--- tests/DateTimeImmutableTest.php
+++ tests/DateTimeImmutableTest.php
@@ -2,10 +2,40 @@
 
 namespace Horloger;
 
+use Horloger\Fixtures\ImmutableContractSubmissionDate;
+
 /**
  * @author Joris Garonian <joris.garonian@gmail.com>
  */
-class DateTimeImmutableTest
+class DateTimeImmutableTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     **/
+    public function it_should_instantiate_an_immutable_date_time_at_a_specific_date()
+    {
+        $date = new ImmutableContractSubmissionDate();
+        sleep(1);
 
+        $this->assertNotEquals(new ImmutableContractSubmissionDate(), $date);
+
+        PredefinedTime::add(ImmutableContractSubmissionDate::class, '1990-05-19');
+
+        $date = new ImmutableContractSubmissionDate();
+        sleep(1);
+
+        $this->assertEquals(new ImmutableContractSubmissionDate(), $date);
+
+        PredefinedTime::stop();
+
+        $this->assertNotEquals(new ImmutableContractSubmissionDate(), $date);
+
+        PredefinedTime::start();
+
+        $this->assertEquals(new ImmutableContractSubmissionDate(), $date);
+
+        PredefinedTime::clean();
+
+        $this->assertNotEquals(new ImmutableContractSubmissionDate(), $date);
+    }
 }
